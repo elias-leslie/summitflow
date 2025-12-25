de7b3f84ca22206e7d287c0aaa5d9c9946c729ec
SummitFlow Dev <kasadis@summitflow.dev>
2025-12-24 21:47:03 -0500

    checkpoint: auto-save at 92% context

--- frontend/components/terminal/keyboard/FullKeyboard.tsx
+++ frontend/components/terminal/keyboard/FullKeyboard.tsx
@@ -15,21 +15,21 @@ const layout = {
     "q w e r t y u i o p",
     "a s d f g h j k l",
     "{shift} z x c v b n m {bksp}",
-    "{sym} ' {space} . {enter}",
+    "{sym} , {space} . {enter}",
   ],
   shift: [
     "1 2 3 4 5 6 7 8 9 0",
     "Q W E R T Y U I O P",
     "A S D F G H J K L",
     "{shift} Z X C V B N M {bksp}",
-    "{sym} ' {space} . {enter}",
+    "{sym} , {space} . {enter}",
   ],
   symbols: [
     "! @ # $ % ^ & * ( )",
-    "- _ = + [ ] \\ | / ~",
+    "- _ = + [ ] \\ ' / ~",
     "` < > { } : ; \" ?",
     "{shift} € £ ¥ • ° ± § {bksp}",
-    "{abc} ' {space} , {enter}",
+    "{abc} | {space} , {enter}",
   ],
 };
 
